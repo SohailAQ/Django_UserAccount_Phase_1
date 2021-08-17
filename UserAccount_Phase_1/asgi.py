6195db8b4b1d987b615216bc3ad1595e31ba21ac
SohailAQ <sohail.developes@gmail.com>
2021-08-17 18:43:50 +0530

    Project name refined.

--- UserAccount_Phase_1/asgi.py
+++ UserAccount_Phase_1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Django_UserAccount_Phase_1 project.
+ASGI config for UserAccount_Phase_1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_UserAccount_Phase_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UserAccount_Phase_1.settings')
 
 application = get_asgi_application()
--- UserAccount_Phase_1/wsgi.py
+++ UserAccount_Phase_1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Django_UserAccount_Phase_1 project.
+WSGI config for UserAccount_Phase_1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_UserAccount_Phase_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UserAccount_Phase_1.settings')
 
 application = get_wsgi_application()
